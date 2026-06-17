eee46f8f3f5670a1ea5f526002edb9397538fe5a
Angad <angadsankhla09@gmail.com>
2026-06-17 23:41:44 +0530

    multi turn conversation added

--- commands/agent.ts
+++ commands/agent.ts
@@ -1,11 +1,28 @@
 import { Command } from 'commander'
 import { agentLoop } from './agent-loop.ts'
+import readline from "readline"
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
 export const agentCommand = new Command("agent")
   .description('Runs the agent')
   .option('-p, --prompt <prompt>', 'prompt', '')
-  .action((options) => {
-    agentLoop(options.prompt)
-    console.log("User prompt is ..." + options.prompt)
-  })
+  .action(async (options) => {
+    while (true) {
+      const answer = await ask("You: ")
+      await agentLoop(answer)
+    }
+  }
+  )
 
+
+async function ask(question: string): Promise<string> {
+  return new Promise((resolve) => {
+    rl.question("You: ", (answer) => {
+      resolve(answer)
+    })
+  })
+}
